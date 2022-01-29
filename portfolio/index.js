3f3eda27a7f60ef6faf88a8810f46dd4e84619b8
Tatyana Ergina <homhom1299@gmail.com>
2022-01-29 15:12:00 +0300

    refactor: fix bags in portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,24 +1,35 @@
-// смена изображений
+//burger-menu
+/*(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header_nav');
+  const menuCloseItem = document.querySelector('.header_nav_close ');
+  burgerItem.addEventListener('click', () => {
+      menu.classList.add('header_nav_active');
+  })
+  menuCloseItem.addEventListener('click', () => {
+   menu.classList.remove('header_nav_active');
+  })
+}());*/
+
+
 const portfolioBtn = document.querySelector('.portfolio-btn');
 const portfolioImages = document.querySelectorAll('.portfolio-image');
 const portfolioBtns = document.querySelectorAll('.portfolio-btns');
-
 console.log(portfolioImages);
 
-
+//slider
 function changeImage(event) {
   if(event.target.classList.contains('portfolio-btn')) {
     const season = event.target.dataset.season;
     console.log(season);
     portfolioImages.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
-    }
+  }
 }
-
 portfolioBtns.forEach(
   function (portfolioBtn) {
     portfolioBtn.addEventListener('click', (changeImage));
   }
-);
+)
 
 
   
