76eeed28fe1b079986c3616191ee3f8a8c7fd0d6
Tatyana Ergina <homhom1299@gmail.com>
2022-02-03 19:46:22 +0300

    fix: update css file

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,5 @@
 //burger-menu
-/*(function () {
+(function () {
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.header_nav');
   const menuCloseItem = document.querySelector('.header_nav_close ');
@@ -9,19 +9,17 @@
   menuCloseItem.addEventListener('click', () => {
    menu.classList.remove('header_nav_active');
   })
-}());*/
+}());
 
-
+//slider
 const portfolioBtn = document.querySelector('.portfolio-btn');
 const portfolioImages = document.querySelectorAll('.portfolio-image');
 const portfolioBtns = document.querySelectorAll('.portfolio-btns');
-console.log(portfolioImages);
+const seasons = ['winter', 'spring', 'summer', 'autumn']; 
 
-//slider
 function changeImage(event) {
   if(event.target.classList.contains('portfolio-btn')) {
     const season = event.target.dataset.season;
-    console.log(season);
     portfolioImages.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
   }
 }
@@ -31,13 +29,48 @@ portfolioBtns.forEach(
   }
 )
 
+//image caching
+/*function preloadSummerImages() {
+    for(let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./assets/img/${seasons}/${i}.jpg`;
+    }
+  }
+  preloadSummerImages();*/
 
+
+//buttons-portfolio backlight
+  //const portfolioBtn = document.querySelector('.portfolio-btn');
+  
+  //const portfolioBtns = document.querySelectorAll('.portfolio-btns');
+ 
+  
+  /*function changeClassActive(event) {
+    if(event.target.classList.contains('portfolio-btn')) {
+      
+      portfolioBtn.forEach.remove('portfolio-btn_active');
+    }
+  }
+  portfolioBtns.forEach(
+    function (portfolioBtn) {
+      portfolioBtn.addEventListener('click', (changeImage));
+    }
+  )*/
+
+  function changeClassActive (event) {
+    
+  }
+  
+  
+  portfolioBtns.forEach(
+    function (portfolioBtn) {
+      portfolioBtn.addEventListener('click', (changeClassActive));
+    }
+  )
+  
+  
+    
   
 
-//function preloadSummerImages() {
-   // for(let i = 1; i <= 6; i++) {
-     // const img = new Image();
-     // img.src = `./assets/img/${seasons}/${i}.jpg`;
-    //}
-  //}
- // preloadSummerImages();
+
+
