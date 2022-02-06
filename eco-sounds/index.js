ceb65634dcec54bd65a7050703a2fad5fdc7a490
Tatyana Ergina <homhom1299@gmail.com>
2022-02-06 23:02:22 +0300

    feat: add audio-player

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,13 +1,17 @@
 //смена изображений при клике
 const audioBtn = document.querySelector('.nav-item');
-const audioImages = document.querySelectorAll('.audio_image');
+const audioImages = document.querySelector('.audio_image');
 const audioBtns = document.querySelectorAll('.nav-items');
+const audio = document.querySelector('audio');
+const playBtn = document.querySelector('.play-btn');
 
 function changeImage(event) {
   if(event.target.classList.contains('nav-item')) {
-    const image = event.target.dataset.image;
-    audioImages.forEach((img) => img.src = `./assets/img/${image}.jpg`);
+    const image = event.target.dataset.item;
+    audioImages.src = `./assets/img/${image}.jpg`;
+    
   }
+
 }
 audioBtns.forEach(
   function (audioBtn) {
@@ -15,16 +19,27 @@ audioBtns.forEach(
   }
 )
 //аудиоплеер
-const audio = document.querySelector('audio');
-const playBtn = document.querySelector('.play-btn');
-//const pauseBtn = document.querySelector('.pause-btn');
+
+function changeAudio(event) {
+  if(event.target.classList.contains('nav-item')) {
+    const bird = event.target.dataset.sound;
+    audio.src = `./assets/audio/${bird}.mp3`;
+    
+  }
+}
+
+audioBtns.forEach(
+  function (audioBtn) {
+    audioBtn.addEventListener('click', (changeAudio));
+  }
+)
 let isPlay = false;
 
 function playAudio() {
   if(!isPlay) {
     audio.currentTime = 0;
-   audio.play();
-   isPlay = true;
+    audio.play();
+    isPlay = true;
 
   } else {
       audio.pause();
@@ -36,32 +51,13 @@ function playAudio() {
 playBtn.addEventListener('click', playAudio);
 
 
-
+//смена кнопок при клике
 function toggleBtn() {
   playBtn.classList.toggle('pause');
 }
 
 playBtn.addEventListener('click', toggleBtn);
 
-//pauseBtn.addEventListener('click', pauseAudio);
-/*function playAudio() {
-  audio.currentTime = 0;
-  audio.play();
-  isPlay = true;
-}
-function pauseAudio() {
-  audio.pause();
-  isPlay = false;
-}
-
-playBtn.addEventListener('click', playAudio);
-pauseBtn.addEventListener('click', pauseAudio);*/
 
 
-/* playAudio() {
-   if(!isPlay) {
-     playAudio();
-   } else {
-     pauseAudio();
-   }
-}*/
+
