58ef8e3ee4ed970809a5ab65ef00e9399a01f0fb
Tatyana Ergina <homhom1299@gmail.com>
2022-02-21 18:34:02 +0300

    feat: add game logic

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,7 +1,56 @@
 const card = document.querySelectorAll('.memory_card');
-
-function flipCard() {
-  this.classList.toggle('flip');
-}
+let ClickedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
 card.forEach(card => card.addEventListener('click', flipCard));
+
+function flipCard() {
+  if(lockBoard) return;
+  if(this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if(!ClickedCard) {
+    ClickedCard = true;
+    firstCard = this;
+    return;
+
+  } else {
+    secondCard = this;
+
+    matchingCards();
+} 
+}
+
+function matchingCards() {
+  if(firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+ } else {
+  unflipCards();
+}
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard () {
+  [ClickedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
